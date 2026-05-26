b90d835235364e8dc7f931b6eca539084e62cbaa
mvpcraft <goldstar105000117@gmail.com>
2026-05-26 17:23:07 -0400

    fix: fix build issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route, Navigate, type ReactNode } from 'react-router-dom'
+import type { ReactNode } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import Privacy from './pages/Privacy'
